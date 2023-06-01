4190654633a13267034d34967284540689cfe218
Bhargav Reddy Godala <bgodala@princeton.edu>
2023-06-01 06:05:27 +0000

    Changing EFI path in m1 run script

--- run_ubuntu_m1.sh
+++ run_ubuntu_m1.sh
@@ -3,4 +3,4 @@ qemu-system-aarch64 -nographic  -accel hvf -machine virt,highmem=off -m 16G -cpu
 -monitor telnet::45454,server,nowait \
 -drive file=imgs/ubuntu-arm.img,if=none,id=drive0,cache=writeback -device virtio-blk-device,indirect_desc=false,event_idx=false,drive=drive0,bootindex=0 \
 -nic none \
--bios scripts/qemu/QEMU_EFI.fd
+-bios scripts/qemu/QEMU_EFI_m1.fd
