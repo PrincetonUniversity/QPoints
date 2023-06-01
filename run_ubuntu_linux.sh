d38991d780e794c53dec63f83d0dd458f0de6be6
Bhargav Reddy Godala <bgodala@princeton.edu>
2023-06-01 20:37:59 +0000

    Use system provided bios file

--- run_ubuntu_linux.sh
+++ run_ubuntu_linux.sh
@@ -4,7 +4,7 @@ qemu-system-aarch64 -nographic -machine virt,gic-version=3 -m 16384M -cpu cortex
 -qmp tcp:localhost:4444,server,nowait -monitor telnet::45454,server,nowait \
 -drive file=imgs/ubuntu-arm.img,if=none,id=drive0,cache=writeback -device virtio-blk-device,indirect_desc=false,event_idx=false,drive=drive0,bootindex=0 \
 -nic none \
--bios scripts/qemu/QEMU_EFI.fd
+-bios /usr/share/qemu-efi-aarch64/QEMU_EFI.fd
 #-kernel kernel_arm_built_img -append "root=/dev/vda2 ro  splash quiet" \
 #-initrd initrd.img \
 
