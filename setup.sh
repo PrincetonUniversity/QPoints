3e4e8d2ae38d87fd3c7cba9db6e28893073090f7
Bhargav Reddy Godala <bgodala@princeton.edu>
2023-06-01 18:43:29 +0000

    Adding setup script to obtain arm kernel image

--- setup.sh
+++ setup.sh
@@ -14,5 +14,12 @@ echo done
 echo Pull gem5 docker image
 docker pull gcr.io/gem5-test/ubuntu-20.04_all-dependencies:v22-1
 bash gem5_docker_build.sh
+cd ..
+
+echo Getting ARM kernel image files for gem5
+cd bin/m5
+wget http://dist.gem5.org/dist/v22-0/arm/aarch-system-20220707.tar.bz2
+tar -xvf aarch-system-20220707.tar.bz2
+cd ../../
 
 echo On Linux host run create_checkpoint.sh script to generate collect a checkpoint
